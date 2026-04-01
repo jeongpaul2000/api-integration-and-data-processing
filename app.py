46ebfc65d3e54430103fe0fc387a35438c006d88
Paul Jeong <jeongpaul2000@gmail.com>
2026-04-01 00:48:06 -0700

    (U) random changeS

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 # Defines an endpoint with hello_world as the path 
 @app.route("/hello_world", methods=["GET"])
 
-# Returns the JSON response for the simple API with a 200 status code saying it was successful
 def hello_world():
     return jsonify({"hello": "world"}), 200
 
